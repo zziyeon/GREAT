0a92e1fc3695bfb345539d1b644f673783df43d9
jiyeon Kim <105837504+zziyeon@users.noreply.github.com>
2022-09-23 22:55:07 +0900

    Update UpdateForm.java
    Signed-off-by: jiyeon Kim <105837504+zziyeon@users.noreply.github.com>

--- src/main/java/com/kh/great/web/product/dto/UpdateForm.java
+++ src/main/java/com/kh/great/web/product/dto/UpdateForm.java
@@ -2,8 +2,6 @@ package com.kh.great.web.product.dto;
 
 import lombok.Data;
 
-import java.util.Date;
-
 @Data
 public class UpdateForm {
     private Long pNumber;           //상품번호    P_NUMBER	NUMBER(6,0)
@@ -11,7 +9,7 @@ public class UpdateForm {
 //    private String storeName;       //가게명    STORE_NAME	VARCHAR2(60 BYTE)
     private String pTitle;          //상품 제목    P_TITLE	VARCHAR2(90 BYTE)
     private String pName;           //상품명    P_NAME	VARCHAR2(60 BYTE)
-    private Date deadlineTime;    //마감일자    DEADLINE_TIME	DATE
+    private String deadlineTime;    //마감일자    DEADLINE_TIME	DATE
     private String pCategory;      //업종카테고리    CATEGORY	VARCHAR2(17 BYTE)
     //    private Integer totalCount;     //총수량    TOTAL_COUNT	NUMBER(5,0)
     private Integer remainCount;    //남은 수량    REMAIN_COUNT	NUMBER(5,0)
