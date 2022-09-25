b8a16f48a044071d4315dd22ed645eb84dc771d7
jiyeon Kim <105837504+zziyeon@users.noreply.github.com>
2022-09-25 13:33:21 +0900

    상품조회 where 조건식 수정
    Signed-off-by: jiyeon Kim <105837504+zziyeon@users.noreply.github.com>

--- src/main/java/com/kh/great/domain/dao/product/ProductDAOImpl.java
+++ src/main/java/com/kh/great/domain/dao/product/ProductDAOImpl.java
@@ -71,7 +71,7 @@ public class ProductDAOImpl implements ProductDAO {
         StringBuffer sql = new StringBuffer();
         sql.append("select  *  ");
         sql.append("from product_info P, member M ");
-        sql.append("where p.owner_number = m.mem_number and p.owner_number=9 ");
+        sql.append("where p.owner_number = m.mem_number and p.p_number=? ");
 
         Product product = null;
 
@@ -84,7 +84,7 @@ public class ProductDAOImpl implements ProductDAO {
                     product.setMember(member);
                     return product;
                 }
-            });
+            }, pNum);
         }catch (DataAccessException e) {
             log.info("조회할 상품이 없습니다. 상품번호={}", pNum);
         }
