0a2ddcee969b2dee4135dc7d0c415c2af7855dc1
leekione <109787841+leekione@users.noreply.github.com>
2022-10-05 19:19:46 +0900

    한개 누락 (#34)
    * 221005
    
    * 221005
    
    * 221005
    
    * 221005

---
+++ src/main/java/com/kh/great/web/dto/mypage/MemberForm.java
@@ -0,0 +1,27 @@
+package com.kh.great.web.dto.mypage;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class MemberForm {
+    private Long memNumber;                   //varchar2(9)
+    private String memType;                     //varchar2(15)
+    private String memId;                       //varchar2(30)
+    private String memPassword;                 //varchar2(18)
+    private String memName;                     //varchar2(18)
+    private String memNickname;                 //varchar2(18)
+    private String memEmail;                    //varchar2(30)
+    private String memBusinessnumber;           //varchar2(10)
+    private String memStoreName;                //varchar2(45)
+    private String memStorePhonenumber;         //varchar2(15)
+    private String memStoreLocation;            //varchar2(150)
+    private String memStoreLatitude;            //varchar2(15, 9)
+    private String memStoreLongitude;           //varchar2(15, 9)
+    private String memStoreIntroduce;           //varchar2(150)
+    private String memStoreSns;                 //varchar2(150)
+    private LocalDateTime memRegtime;           //date
+    private LocalDateTime memLockExpiration;    //date
+    private String memAdmin;
+}
