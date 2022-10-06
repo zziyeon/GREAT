d9e283ab5852cc7eee276ae631e09082e84ce789
Junhyeok Heo <106042161+Heoveloper@users.noreply.github.com>
2022-10-06 17:28:33 +0900

    valid check (#41)
    * valid check
    
    * valid check

--- src/main/resources/static/js/member/infoOwn.js
+++ src/main/resources/static/js/member/infoOwn.js
@@ -272,6 +272,75 @@ const callback = function(result, status) {
 };
 
 
+//수정완료 버튼
+$modifyBtn = document.querySelector('.modify-btn');
+//점주회원정보 수정 폼
+$infoForm = document.querySelector('.info');
+
+//가입하기 버튼 클릭시 호출되는 함수
+function btnClick() {
+
+    //유효성 검사
+    if (!isValidChk()) {
+        return;
+    }
+
+    //회원가입
+    $infoForm.submit();
+}
+
+
+//유효성 검사 함수
+function isValidChk() {
+    console.log(memNumber);
+//    if ($dupChkId.classList[2] != 'good') {
+//        alert('아이디 중복확인이 필요합니다.');
+//        return false;
+//    } else if ($dupChkNn.classList[2] != 'good') {
+//        alert('닉네임 중복확인이 필요합니다.');
+//        return false;
+//    } else if ($sendCodeBtn.classList[2] == 'bad') {        //나중에 바꿀 거 바꾸고 !=good으로 바꿔줘야함
+//        alert ('메일주소를 다시 확인해주세요.');
+//        return false;
+//    } else if ($confirmCodeBtn.classList[2] == 'bad') {     //나중에 바꿀 거 바꾸고 !=good으로 바꿔줘야함
+//        alert('인증번호 확인이 필요합니다.');
+//        return false;
+//    } else if (!($termsAll.checked)) {
+//        alert('약관 전체동의가 필요합니다.');
+//        return false;
+//    }
+//
+//    //점주회원가입이면 유효성 검사
+//    //고객회원이 아니면
+//    else if (!($joinCust.checked)) {
+//        if ($bnConfirmBtn.classList[2] != 'good') {
+//            alert('사업자번호 인증이 필요합니다.');
+//            return false;
+//        }
+//        if (memStoreName.value == '') {
+//            alert('가게명은 필수입력사항입니다.');
+//            return false;
+//        }
+//        if (memStorePhonenumber.value == '') {
+//            alert('가게 연락처는 필수입력사항입니다.');
+//            return false;
+//        }
+//        if (memStoreLocation.value == '') {
+//            alert('가게 주소는 필수입력사항입니다.');
+//            return false;
+//        }
+//        const $detailedAddress = document.querySelector(".detailed-address");
+//        if ($detailedAddress.value == '') {
+//            alert('가게 상세주소를 입력해주세요.');
+//            return false;
+//        }
+//        return true;
+//    }
+
+    return true;
+}
+
+
 //회원탈퇴 버튼
 const $exitBtn = document.querySelector('.exit-btn');
 
