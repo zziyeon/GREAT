77f5ae84e8169b401d1d0534f3e970b740446a8c
zziyeon <jiyunsara99@naver.com>
2022-10-08 11:14:35 +0900

    addForm 에서 max 구현중

--- src/main/resources/static/js/product/limitDate.js
+++ src/main/resources/static/js/product/limitDate.js
@@ -1,5 +1,9 @@
 const dateElement = document.getElementById('currentTime');
 const date = new Date(new Date().getTime() - new Date().getTimezoneOffset() * 60000).toISOString().slice(0, -8);
+console.log(date);
+const date1 = new Date(new Date().getTime()).toISOString().slice(0, -8);
+console.log(date1);
 //dateElement.value = date;
 //date=date.substr(0,20);
 dateElement.setAttribute("min", date);
+dateElement.setAttribute("max", date1);
