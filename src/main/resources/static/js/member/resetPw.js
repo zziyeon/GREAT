0776bdb766b2b43b9d56f1a40bbfd8f173941227
Junhyeok Heo <106042161+Heoveloper@users.noreply.github.com>
2022-10-11 12:43:26 +0900

    valid chk -ing

--- src/main/resources/static/js/member/resetPw.js
+++ src/main/resources/static/js/member/resetPw.js
@@ -10,11 +10,19 @@ function close() {
 
 //재설정 완료 버튼
 $confirmBtn = document.querySelector('.confirm-btn');
+//비밀번호 재설정 에러메세지
+$resetPwEmpty = document.querySelector('.resetPwEmpty');
 
 //재설정 완료 버튼 클릭시
 $confirmBtn.addEventListener('click', e => {
     console.log("재설정 완료 버튼 클릭!");
 
+    //유효성 검사
+    if (!isValidChk()) {
+        return;
+    }
+
+    const ids = memId.value;
     //재설정할 비밀번호값
     const pwVal = memPassword.value;
     if (pwVal == null) {alert('재설정할 비밀번호를 입력하세요.');}
@@ -32,6 +40,39 @@ $confirmBtn.addEventListener('click', e => {
     //비밀번호 재설정
     resetPw(member);
 });
+
+//비밀번호 형식 검사 함수
+function pwFormat() {
+        //const password = /^[a-z0-9_-]{8,15}$/;
+        const pwFormat = /^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,15}$/;
+
+
+        if (!pwFormat.exec(memPassword.value)) {
+            return false;
+        }
+        return true;
+}
+
+//유효성 검사 함수
+function isValidChk() {
+    if (memId.value == '' || memPassword.value == '' || memPasswordCheck.value == '') {
+        $resetPwEmpty.classList.add('reveal');
+        resetPwErrmsg.textContent = '재설정할 비밀번호를 입력하세요.';
+        return false;
+    }
+    if (!pwFormat()) {
+        $resetPwEmpty.classList.add('reveal');
+        resetPwErrmsg.textContent = '비밀번호 형식이 맞지 않습니다.';
+        return false;
+    }
+    if (memPassword.value != memPasswordCheck.value) {
+        $resetPwEmpty.classList.add('reveal');
+        resetPwErrmsg.textContent = '비밀번호가 일치하지 않습니다.';
+        return false;
+    }
+
+    return true;
+}
 
 //재설정 완료 함수
 function resetPw(member) {
